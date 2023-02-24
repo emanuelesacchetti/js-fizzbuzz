5cfeb6190ac70f3a61e8526b92f815646372a10f
emanuele sacchetti <manusacco92@gmail.com>
2023-02-24 15:39:36 +0100

    programma funzionante in console.log

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
-    
-    /*if (i % 3 == 0){
-        i = 'Fizz'
-    } else if (i % 5 == 0){
-        i = 'Buzz'
-    } else if (i % (3 * 5) == 0){
-        i = 'FizzBuzz'
-    }*/
 
-    
+    if (i % (3 * 5) == 0){
+        console.log('fizzBuzz')
+    } else if (i % 3 == 0) {
+        console.log('fizz')
+    } else if (i % 5 == 0){
+        console.log('buzz')
+    } else {
+        console.log(i)
+    }
+   
 }
